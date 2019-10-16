421d14527f777651c98bfcfdd4b952a9b4eaa2ba
Martinpereyra <pereyramartinn@gmail.com>
2019-10-15 22:21:31 -0300

    Logic Adapter

---
+++ Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic : BusinessLogic
+    {
+        private UsuarioAdapter _usuarioData;
+
+        public UsuarioAdapter UsuarioData
+        {
+            get { return _usuarioData; }
+            set { _usuarioData = value; }
+        }
+
+        public UsuarioLogic()
+        {
+            UsuarioData = new UsuarioAdapter();
+        }
+
+
+        
+    }
+}
---
+++ UI.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Business.Logic;
+using Data.Database;
+
+
+namespace UI.Console
+{
+    public class Usuarios
+    {
+        private UsuarioLogic usuarioLogic;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
